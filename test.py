d107dd5a131d3b8ee4b8a2df19138a7dec438254
Emil Madsen <sovende@gmail.com>
2020-10-03 17:40:20 +0200

    Reformatting

--- test.py
+++ test.py
@@ -1,25 +1,27 @@
 import asyncio
 import signal
 import sys
-
-from uvicorn import Config, Server
-import pynmea2
-from datetime import time, datetime, date
-from prometheus_client import make_asgi_app, Gauge
 from collections import defaultdict
+from datetime import date, datetime, time
 
+import pynmea2
+from prometheus_client import Gauge, make_asgi_app
 from pynmea2.types.talker import TalkerSentence
+from uvicorn import Config, Server
+
 
 def generate_metrics(subclass):
     class_name = subclass.__name__
+
     def generate_metric(field):
         help_text = field[0]
         short_name = field[1]
         metric_name = class_name + "_" + field[1]
-        metric_name = metric_name.replace(' ', '_')
+        metric_name = metric_name.replace(" ", "_")
         return short_name, Gauge(metric_name, help_text)
 
     return class_name, dict(map(generate_metric, subclass.fields))
+
 
 message_types = TalkerSentence.__subclasses__()
 metrics = dict(map(generate_metrics, message_types))
@@ -34,7 +36,9 @@ def install_signal_handlers(loop):
     for sig in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(sig, sys.exit, sig)
 
+
 last_timestamp = None
+
 
 async def handle_timestamp(timestamp):
     global last_timestamp
@@ -43,14 +47,14 @@ async def handle_timestamp(timestamp):
         last_timestamp = timestamp
         return
 
-    difference = (
-        datetime.combine(date.today(), timestamp) -
-        datetime.combine(date.today(), last_timestamp)
+    difference = datetime.combine(date.today(), timestamp) - datetime.combine(
+        date.today(), last_timestamp
     )
     sleep_seconds = difference.total_seconds()
     if sleep_seconds > 0:
         await asyncio.sleep(sleep_seconds)
     last_timestamp = timestamp
+
 
 async def handle_message(msg):
     print(repr(msg))
@@ -67,17 +71,18 @@ async def handle_message(msg):
             continue
         metrics[msg.sentence_type][short_name].set(value)
 
-    if hasattr(msg,"timestamp"):
+    if hasattr(msg, "timestamp"):
         await handle_timestamp(msg.timestamp)
 
+
 async def main():
-    with open("plaka.log", encoding='utf-8') as nema_file:
+    with open("plaka.log", encoding="utf-8") as nema_file:
         for line in nema_file.readlines():
             try:
                 msg = pynmea2.parse(line, check=True)
                 await handle_message(msg)
             except pynmea2.ParseError as e:
-                print('Parse error: {}'.format(e))
+                print("Parse error: {}".format(e))
                 continue
 
 
